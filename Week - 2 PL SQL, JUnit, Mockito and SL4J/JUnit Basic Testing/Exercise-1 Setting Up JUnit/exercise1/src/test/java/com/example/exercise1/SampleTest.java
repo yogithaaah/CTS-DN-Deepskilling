232dc06730acd31a9c0f13ff007aa6c4bea3fec3
yogithaaah <ytippireddy@gmail.com>
2025-06-25 23:14:20 +0530

    week2 junit ex1

---
+++ Week - 2 PL SQL, JUnit, Mockito and SL4J/JUnit Basic Testing/Exercise-1 Setting Up JUnit/exercise1/src/test/java/com/example/exercise1/SampleTest.java
@@ -0,0 +1,13 @@
+package com.example.exercise1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+
+    @Test
+    public void testAddition() {
+        int sum = 5 + 7;
+        assertEquals(12, sum);
+    }
+}
