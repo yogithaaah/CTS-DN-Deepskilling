f750f94394183a138e180abe717eb81e8d32d2f9
yogithaaah <ytippireddy@gmail.com>
2025-07-04 20:59:37 +0530

    spring core and maven exercises

---
+++ Week 3 - Spring Core/Spring Core& Maven/Exercise-1 Configuring a Basic Spring Application/LibraryManagement/src/main/java/com/library/LibraryManagement/App.java
@@ -0,0 +1,16 @@
+package com.library.LibraryManagement;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class App 
+{
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService service = (BookService) context.getBean("bookService");
+        service.printBook();
+    }
+}
---
+++ Week 3 - Spring Core/Spring Core& Maven/Exercise-2 Implementing Dependency Injection/LibraryManagement2/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+	 public String getBookTitle() {
+	        return "Spring in Action";
+	    }
+}
