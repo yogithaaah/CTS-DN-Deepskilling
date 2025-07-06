47a040b1561ada9f1b9965fa714596db56c60540
yogithaaah <ytippireddy@gmail.com>
2025-07-06 18:20:42 +0530

    completed week3

---
+++ Week 3 - Spring Core/Spring Data JPA/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,33 @@
+package com.cognizant.orm_learn;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+	private static CountryService countryService;
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+		LOGGER.info("Inside main");
+		
+		countryService = context.getBean(CountryService.class);
+		testGetAllCountries();
+	}
+
+	private static void testGetAllCountries() {
+		LOGGER.info("Start");
+		List<Country> countries = countryService.getAllCountries();
+		LOGGER.debug("countries={}", countries);
+		LOGGER.info("End");
+	}
+}
---
+++ Week 3 - Spring Core/Spring Data JPA/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -0,0 +1,21 @@
+package com.cognizant.orm_learn.service;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
