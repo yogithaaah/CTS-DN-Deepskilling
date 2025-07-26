e31c6f606c3a45431379b10a71cffb08873c0be8
yogithaaah <ytippireddy@gmail.com>
2025-07-26 12:32:41 +0530

    week 6

---
+++ Week6-React/Handson-2/studentapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./Components/Home"
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+
+
+function App() {
+  return (
+    <div className="container">
+      <Home/>
+      <About/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
